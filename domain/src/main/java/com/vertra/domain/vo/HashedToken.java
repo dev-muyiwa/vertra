945e57dd5f9f3aaaac3181a9e0b35e2929f249f9
dev-muyiwa <dev.muyiwa@gmail.com>
2025-11-14 08:12:21 +0100

    Add null check and override toString method in HashedToken

--- domain/src/main/java/com/vertra/domain/vo/HashedToken.java
+++ domain/src/main/java/com/vertra/domain/vo/HashedToken.java
@@ -1,9 +1,16 @@
 package com.vertra.domain.vo;
 
+import org.jetbrains.annotations.NotNull;
+
 public record HashedToken(String value) {
     public HashedToken {
         if (value == null) {
             throw new IllegalArgumentException("Invalid token hash");
         }
+    }
+
+    @Override
+    public @NotNull String toString() {
+        return value;
     }
 }
